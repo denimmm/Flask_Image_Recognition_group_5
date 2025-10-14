c211df0c3595640be6bf422ca392259a192c3f4d
Aiden-A-Wilson <awilson9717@conestogac.on.ca>
2025-10-14 11:41:35 -0400

    set up pylint file
    I have made the pylint file you can run this by doing 'python lint.py' in terminal

---
+++ lint.py
@@ -0,0 +1,21 @@
+#lint.py 
+
+import sys 
+
+from pylint import lint  
+
+THRESHOLD = 9  
+
+run = lint.Run(["app.py"])
+
+
+score = run.linter.stats["global_note"]  
+
+if score < THRESHOLD: 
+
+    print("Linter failed: Score < threshold value") 
+
+    sys.exit(1) 
+
+
+sys.exit(0) 
